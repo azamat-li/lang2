0d14032a350293fb3fc657bb6d9f198c7ba5f88f
azamat-li <azamatalifullstack@gmail.com>
2021-01-27 15:30:08 +0500

    feat: post page added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostsController ;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
+
+Route::get('/posts', [PostsController::class, 'main'])->name('posts.main');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
