128f01d5afd6c2d728c19f4b97224fc48e9f515b
azamat-li <azamatalifullstack@gmail.com>
2021-01-31 00:12:17 +0500

    style: removed second navbar

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -75,28 +75,6 @@
   </head>
 
   <body class="gradient leading-relaxed tracking-wide flex flex-col">
-		<nav id="header" class="fixed w-full z-10 top-0">
-
-			<div id="progress" class="h-1 z-20 top-0" style="background:linear-gradient(to right, #4dc0b5 var(--scroll), transparent 0);"></div>
-
-			<div class="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
-
-				<div class="pl-4">
-					<a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl" href="#">
-					{{ config('app.name') }}
-					</a>
-				</div>
-
-				<div class="block lg:hidden pr-4">
-					<button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-green-500 appearance-none focus:outline-none">
-					</button>
-				</div>
-
-				<div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-gray-100 md:bg-transparent z-20" id="nav-content">
-
-				</div>
-			</div>
-		</nav>
 
     <!--Nav-->
     <nav id="header" class="w-full z-30 top-0 text-white py-1 lg:py-6">
